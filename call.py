1aef17c8b5d36bd513f7d5db4b213ea4337ec546
Son Nguyen <ntson2002@gmail.com>
2017-03-16 15:57:57 +0900

    Upload

---
+++ call.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+
+s1 = "Phó thủ tướng Malaysia Ahmad Zahid Hamidi hôm qua cho biết thi thể ông Kim Jong-nam đã được xác định dựa trên mẫu ADN con ông cung cấp."
+s2 = "Phó thủ tướng Malaysia Ahmad Zahid Hamidi hôm qua cho biết thi thể ông Kim Jong-nam đã được xác định dựa trên mẫu ADN con ông cung cấp."
+
+data = "\n".join([s1, s2])
+
+r = requests.post("http://150.65.242.122:8224/api/tagger/vn/", data=data)
+
+print r.text
